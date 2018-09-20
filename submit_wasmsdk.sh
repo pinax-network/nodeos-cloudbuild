2bea20a9c7c47f248b711943efa209485100e7ae
Matthieu Vachon <matt@eoscanada.com>
2018-09-20 00:43:15 -0400

    Updated eosio.cdt to latest version

--- submit_wasmsdk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-# This first build step only creates the build environment, to be used by the next compile step.
-
-export TAG=v1.1.1
-export EOS_WASMSDK_VERSION=v1.1.1
-export CLOUDSDK_CORE_PROJECT=eoscanada-public
-
-gcloud builds submit . --async --config cloudbuild-wasmsdk.yaml --disk-size 120 --timeout 8h --substitutions "COMMIT_SHA=${TAG},_EOS_WASMSDK_VERSION=${EOS_WASMSDK_VERSION}" --machine-type=n1-highcpu-32
