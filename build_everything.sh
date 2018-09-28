a7d4ebd8f438360ffbed630797837f8527f77693
Stéphane Duchesneau <stephane.duchesneau@eoscanada.com>
2018-09-28 16:06:23 -0400

    add webhook in slack, also handle patch v1.2

--- build_everything.sh
+++ build_everything.sh
@@ -21,8 +21,12 @@ $PRINT "Building Nodeos from $SRC"
 $PRINT "Building prod container for $DST"
 ./submit_nodeos_prod.sh $DST
 
+PATCH=1.3.x
+if echo $SRC | grep ^v1.2; then
+    PATCH=1.2.x
+fi
 $PRINT "Building Nodeos from $SRC with deep-mind"
-./submit_nodeos_full.sh $SRC $DST-deep-mind deep-mind-1.3.x.patch
+./submit_nodeos_full.sh $SRC $DST-deep-mind deep-mind-${PATCH}.patch
 $PRINT "Building prod container for $DST-deep-mind"
 ./submit_nodeos_prod.sh $DST-deep-mind
 
@@ -34,3 +38,11 @@ echo "gcr.io/eoscanada-public/eosio-nodeos-prod:$DST for bp/peering/api nodes"
 echo "-- based on EOSIO/EOS tag $SRC with deep-mind patches --"
 echo "gcr.io/eoscanada-public/eosio-nodeos-full:$DST-deep-mind for dev"
 echo "gcr.io/eoscanada-public/eosio-nodeos-prod:$DST-deep-mind for pusher nodes only"
+
+$PRINT "Don't forget to submit manageos and pusher builds!"
+
+curl -X POST -H 'Content-type: application/json' --data '{"text":"*New Nodeos builds produced! -- based on EOSIO/EOS tag $SRC --*
+* gcr.io/eoscanada-public/eosio-nodeos-full:v1.3.0 for dev 
+* gcr.io/eoscanada-public/eosio-nodeos-prod:v1.3.0 for bp/peering/api nodes
+* gcr.io/eoscanada-public/eosio-nodeos-full:v1.3.0-deep-mind for dev 
+* gcr.io/eoscanada-public/eosio-nodeos-prod:v1.3.0-deep-mind for pusher nodes only"}' https://hooks.slack.com/services/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
