11cce55623ea893f5c520ccebbe4d70522b1c019
Alexandre Bourget <alex@bourget.cc>
2018-10-15 11:24:49 -0400

    Added instrumentation for `canceldelay`, forgot `PUSH_CREATE` in `capture` turbo :deng:

--- deep-mind/boot/run.sh
+++ deep-mind/boot/run.sh
@@ -23,50 +23,60 @@ eosc system newaccount battlefield1 battlefield2 --auth-key EOS5MHPYyhjBjnQZejzZ
 
 sleep 0.6
 
-eosc tx create battlefield1 dbins '{"account": "battlefield1"}' -p battlefield1@active
+eosc tx create battlefield1 dbins '{"account": "battlefield1"}' -p battlefield1
 
 sleep 0.6
 
-eosc tx create battlefield1 dbupd '{"account": "battlefield1"}' -p battlefield1@active
+eosc tx create battlefield1 dbupd '{"account": "battlefield1"}' -p battlefield1
 
 sleep 0.6
 
-eosc tx create battlefield1 dbrem '{"account": "battlefield1"}' -p battlefield1@active
+eosc tx create battlefield1 dbrem '{"account": "battlefield1"}' -p battlefield1
 
 sleep 0.6
 
-eosc tx create battlefield1 dtrx '{"account": "battlefield1", "fail_now": false, "fail_later": false, "delay_sec": 1, "nonce": "1"}' -p battlefield1@active
-eosc tx create battlefield1 dtrxcancel '{"account": "battlefield1"}' -p battlefield1@active
+eosc tx create battlefield1 dtrx '{"account": "battlefield1", "fail_now": false, "fail_later": false, "delay_sec": 1, "nonce": "1"}' -p battlefield1
+eosc tx create battlefield1 dtrxcancel '{"account": "battlefield1"}' -p battlefield1
 
 sleep 0.6
 
-eosc tx create battlefield1 dtrx '{"account": "battlefield1", "fail_now": true, "fail_later": false, "delay_sec": 1, "nonce": "1"}' -p battlefield1@active || true
+eosc tx create battlefield1 dtrx '{"account": "battlefield1", "fail_now": true, "fail_later": false, "delay_sec": 1, "nonce": "1"}' -p battlefield1 || true
 
 sleep 0.6
 
 # `send_deferred` with `replace_existing` enabled, to test `MODIFY` clauses.
-eosc tx create battlefield1 dtrx '{"account": "battlefield1", "fail_now": false, "fail_later": false, "delay_sec": 1, "nonce": "1"}' -p battlefield1@active
-eosc tx create battlefield1 dtrx '{"account": "battlefield1", "fail_now": false, "fail_later": false, "delay_sec": 1, "nonce": "2"}' -p battlefield1@active
+eosc tx create battlefield1 dtrx '{"account": "battlefield1", "fail_now": false, "fail_later": false, "delay_sec": 1, "nonce": "1"}' -p battlefield1
+eosc tx create battlefield1 dtrx '{"account": "battlefield1", "fail_now": false, "fail_later": false, "delay_sec": 1, "nonce": "2"}' -p battlefield1
 
 sleep 0.6
 
-eosc tx create battlefield1 dtrx '{"account": "battlefield1", "fail_now": false, "fail_later": true, "delay_sec": 1, "nonce": "1"}' -p battlefield1@active
+eosc tx create battlefield1 dtrx '{"account": "battlefield1", "fail_now": false, "fail_later": true, "delay_sec": 1, "nonce": "1"}' -p battlefield1
 
 echo Waiting for the transaction to fail...
 
 sleep 1.1
 
-eosc tx create battlefield1 dbinstwo '{"account": "battlefield1", "first": 100, "second": 101}' -p battlefield1@active
+eosc tx create battlefield1 dbinstwo '{"account": "battlefield1", "first": 100, "second": 101}' -p battlefield1
 # This TX will do one DB_OPERATION for writing, and the second will fail. We want our instrumentation NOT to keep that DB_OPERATION.
-eosc tx create --delay-sec=1 battlefield1 dbinstwo '{"account": "battlefield1", "first": 102, "second": 100}' -p battlefield1@active
+eosc tx create --delay-sec=1 battlefield1 dbinstwo '{"account": "battlefield1", "first": 102, "second": 100}' -p battlefield1
 
 echo Waiting for the transaction to fail, yet attempt to write to storage
 sleep 1.1
 
 # This is to see how the RAM_USAGE behaves, when a deferred hard_fails. Does it refund the deferred_trx_remove ? What about the other RAM tweaks? Any one them saved?
-eosc tx create battlefield1 dbinstwo '{"account": "battlefield1", "first": 200, "second": 201}' -p battlefield1@active
+eosc tx create battlefield1 dbinstwo '{"account": "battlefield1", "first": 200, "second": 201}' -p battlefield1
 
 sleep 0.6
+
+echo "Create a delayed and cancel it with 'eosio:canceldelay'"
+eosc tx create --delay-sec=3600 battlefield1 dbins '{"account": "battlefield1"}' -p battlefield1 --write-transaction /tmp/delayed.json
+ID=`eosc tx id /tmp/delayed.json`
+eosc tx push /tmp/delayed.json
+eosc tx cancel battlefield1 $ID
+rm /tmp/delayed.json || true
+
+sleep 0.6
+
 
 # TODO: provode a `soft_fail` transaction
 # TODO: provoke an `expired` transaction. How to do that? Too loaded and can't push it through?
