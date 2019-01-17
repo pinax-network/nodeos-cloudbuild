de6bfc3143dd6b5bda026c89c29ad90df0d71838
Matthieu Vachon <matt@eoscanada.com>
2019-01-17 15:07:40 -0500

    Fixed for deep mind building on BOS

--- submit_bos_deepmind.sh
+++ submit_bos_deepmind.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -xe
 
+SHORT_SHA=`git rev-parse --short HEAD`
+if [[ $? != 0 ]]; then
+    echo "Unable to retrieve git short commit sha1 via 'git rev-parse --short HEAD'"
+    exit 1
+fi
+
 # This step builds `bos-full` & `bos-prod` images.
 export CLOUDSDK_CORE_PROJECT=eoscanada-shared-services
 
 gcloud builds submit . \
           --config cloudbuild-bos-deepmind.yaml \
           --timeout 8h \
-          --machine-type=n1-highcpu-32
+          --machine-type=n1-highcpu-32 \
+          --substitutions SHORT_SHA=${SHORT_SHA}
