c90f9fccc5494ed4dbddcbb3a90139dd5af0b080
Matthieu Vachon <matt@eoscanada.com>
2019-01-17 10:31:40 -0500

    Added bos images building

---
+++ submit_bos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xe
+
+# This step builds `bos-full` & `bos-prod` images.
+export CLOUDSDK_CORE_PROJECT=eoscanada-shared-services
+
+gcloud builds submit . \
+          --config cloudbuild-bos.yaml \
+          --timeout 8h \
+          --machine-type=n1-highcpu-32
