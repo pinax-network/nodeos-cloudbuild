aba97793661c8a705abad90f2446b1f189cf3f1d
Matthieu Vachon <matt@eoscanada.com>
2019-03-13 23:43:42 -0400

    Updated script and update CDT to latest version

--- submit_cdt.sh
+++ submit_cdt.sh
@@ -5,7 +5,16 @@ export CLOUDSDK_CORE_PROJECT=${CLOUDSDK_CORE_PROJECT:-"eoscanada-shared-services
 main() {
     VERSION=${1}
     if [[ ${VERSION} == "" ]]; then
-        echo "ERROR: Missing branch / tag name"
+        echo "ERROR: Missing version"
+        echo ""
+
+        usage
+        exit 1
+    fi
+
+    MODIFIER=${2}
+    if [[ ${MODIFIER} == "" ]]; then
+        echo "ERROR: Missing modifier"
         echo ""
 
         usage
@@ -13,8 +22,8 @@ main() {
     fi
 
     TAG=${VERSION}
-    if [[ ${2} != "" ]]; then
-        TAG=${2}
+    if [[ ${3} != "" ]]; then
+        TAG=${3}
     fi
 
     gcloud builds submit . \
@@ -22,14 +31,15 @@ main() {
     --config cloudbuild-cdt.yaml \
     --disk-size 120 \
     --timeout 8h \
-    --substitutions "_TAG=${TAG},_EOSIO_CDT_VERSION=${VERSION}" \
+    --substitutions "_TAG=${TAG},_EOSIO_CDT_VERSION=${VERSION},_EOSIO_CDT_MODIFIER=${MODIFIER}" \
     --machine-type=n1-highcpu-32
 }
 
 usage() {
-    echo "usage: $0 git-ref [docker-tag]"
-    echo "  git-ref     The git reference (branch/tag/sha1) to build"
-    echo "  docker-tag  The tag to use when tagging the docker image, optional, defaults to 'git-ref'"
+    echo "usage: $0 version modifier [docker-tag]"
+    echo "  version     The version release to use, this is 1.4.1, 1.5.0, etc."
+    echo "  modifier    The release archive modifier to download, for example '-1.amd64' or '.x86_64'"
+    echo "  docker-tag  The tag to use when tagging the docker image, optional, defaults to 'version'"
     echo ""
 }
 
