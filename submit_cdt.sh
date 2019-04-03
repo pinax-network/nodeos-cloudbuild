496a8666778f4635164c116bda3eae241bbe481a
Matthieu Vachon <matt@eoscanada.com>
2019-04-03 00:24:50 -0400

    Updated CDT build

--- submit_cdt.sh
+++ submit_cdt.sh
@@ -12,9 +12,9 @@ main() {
         exit 1
     fi
 
-    MODIFIER=${2}
-    if [[ ${MODIFIER} == "" ]]; then
-        echo "ERROR: Missing modifier"
+    PACKAGE_NAME=${2}
+    if [[ ${PACKAGE_NAME} == "" ]]; then
+        echo "ERROR: Missing package name"
         echo ""
 
         usage
@@ -31,15 +31,15 @@ main() {
     --config cloudbuild-cdt.yaml \
     --disk-size 120 \
     --timeout 8h \
-    --substitutions "_TAG=${TAG},_EOSIO_CDT_VERSION=${VERSION},_EOSIO_CDT_MODIFIER=${MODIFIER}" \
+    --substitutions "_TAG=${TAG},_EOSIO_CDT_VERSION=${VERSION},_EOSIO_CDT_PACKAGE_NAME=${PACKAGE_NAME}" \
     --machine-type=n1-highcpu-32
 }
 
 usage() {
-    echo "usage: $0 version modifier [docker-tag]"
-    echo "  version     The version release to use, this is 1.4.1, 1.5.0, etc."
-    echo "  modifier    The release archive modifier to download, for example '-1.amd64' or '.x86_64'"
-    echo "  docker-tag  The tag to use when tagging the docker image, optional, defaults to 'version'"
+    echo "usage: $0 version package-name [docker-tag]"
+    echo "  version       The version release to use, this is 1.6.0, etc."
+    echo "  package-name  The release archive package name to download, for example 'eosio.cdt_1.6.0-1_amd64.deb'"
+    echo "  docker-tag    The tag to use when tagging the docker image, optional, defaults to 'version'"
     echo ""
 }
 
