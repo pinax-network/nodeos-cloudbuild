450d2e8231764842234cfaf6380eba709279acf5
Matthieu Vachon <matt@dfuse.io>
2020-04-28 08:13:21 -0400

    Renamed eosio-base to eosio-ci-env

---
+++ submit-eosio-ci-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export CLOUDSDK_CORE_PROJECT=eoscanada-shared-services
+
+[[ ! -d $ROOT/build ]] && mkdir $ROOT/build
+
+gcloud builds submit . --config eosio-ci-env.cb.yaml --substitutions _IMAGE_TAG=centos-7.7 --timeout 1h --machine-type=n1-highcpu-32 --async
+gcloud builds submit . --config eosio-ci-env.cb.yaml --substitutions _IMAGE_TAG=ubuntu-16.04 --timeout 1h --machine-type=n1-highcpu-32 --async
+gcloud builds submit . --config eosio-ci-env.cb.yaml --substitutions _IMAGE_TAG=ubuntu-18.04 --timeout 1h --machine-type=n1-highcpu-32 --async
